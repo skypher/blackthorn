7df9239e741408568c1a9f224fc2e30155aa5c72
Elliott Slaughter <elliottslaughter@gmail.com>
2010-03-23 13:57:18 -0700

    Added REFMAP graphics resources.

---
+++ disp/refmap/gettilesets.sh
@@ -0,0 +1,44 @@
+function download () {
+    if [ `which -s wget; echo $?` == 0 ]
+    then
+        wget $1
+    else
+        if [ `which -s curl; echo $?` == 0 ]
+        then
+            curl $1 -o `basename $1`
+        else
+            echo "Can't find wget or curl. Don't know how to download files."
+        fi
+    fi
+}
+
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileA1.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileA2.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileA3.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileA4.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileA5.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileB.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileC.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileD.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/map/TileE.png'
+
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara01_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara01_b.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara02_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara02_b.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara02_c.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara02_d.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_b.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_c.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_d.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_e.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_f.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara03_g.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara04_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara04_b.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara06_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara07_a.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara07_b.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara07_c.png'
+download 'http://www.tekepon.net/fsm/modules/refmap/images/chara/vx_chara08_a.png'
