c4be2b59083119463fac6dc66a5252b8c7de0043
Elliott Slaughter <elliottslaughter@gmail.com>
2010-03-23 17:20:51 -0700

    Removed some characters to reduce size of combined sprite sheet.

--- src/bunnyslayer/driver.lisp
+++ src/bunnyslayer/driver.lisp
@@ -69,7 +69,7 @@
     (setf (game-root game) root
           (game-view game) (make-instance 'component :size size)
           (game-sheet game)
-          (make-instance 'sheet :name :sheet :source (directory (resource "disp/refmap/vx*.png"))))
+          (make-instance 'sheet :name :sheet :source (directory (resource "disp/refmap/*.png"))))
     (let ((hero (make-instance
                  'hero :parent root :offset (/ size 2)
                  :image (make-instance 'image :name :hero))))
